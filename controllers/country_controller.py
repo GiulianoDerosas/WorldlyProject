f80cbf95fd87b585f8f28abd0e06d2e017bf6068
GiulianoDerosas <giulianoderosas@gmail.com>
2021-04-23 18:04:11 +0400

    Added user controller base route

--- controllers/country_controller.py
+++ controllers/country_controller.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask import Blueprint
-from models.city import Country
+from models.country import Country
 import repositories.city_repository as city_repository
 import repositories.country_repository as country_repository
 import repositories.user_repository as user_repository
@@ -8,7 +8,7 @@ import repositories.destination_repository as destination_repository
 
 countries_blueprint = Blueprint("countries", __name__)
 
-@cities_blueprint.route("/countries")
+@countries_blueprint.route("/countries")
 def countries():
     countries = country_repository.select_all()
     return render_template("country/index.html", countries = countries)
