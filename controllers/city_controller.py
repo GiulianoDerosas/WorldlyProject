42c3c7c2236b0277f6f7fcce56c9ae0516cd0bb2
GiulianoDerosas <giulianoderosas@gmail.com>
2021-04-23 17:59:04 +0400

    Added a city controller and created index.html for other controllers

---
+++ controllers/city_controller.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, redirect
+from flask import Blueprint
+from models.city import City
+import repositories.city_repository as city_repository
+
+cities_blueprint = Blueprint("cities", __name__)
+
+@cities_blueprint.route("/cities")
+def cities():
+    cities = city_repository.select_all()
+    return render_template("city/index.html", cities = cities)
