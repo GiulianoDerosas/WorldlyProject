7a5acef8fb84aa31b9625b89d2f66c6c2a5a4aea
GiulianoDerosas <giulianoderosas@gmail.com>
2021-04-23 15:20:47 +0400

    Added three classes with respective attributes

---
+++ models/city.py
@@ -0,0 +1,6 @@
+class City:
+    def __init__(self, country, city_name, blurb, id = None)
+    self.country = country
+    self.city_name = city_name
+    self.blurb = notes
+    self.id = id
---
+++ models/user.py
@@ -0,0 +1,7 @@
+class User:
+
+    def __init__(self, first_name, last_name, favorite_destination, id = None):
+      self.first_name = first_name
+      self.last_name = last_name
+      self.favorite_destination = favorite_destination
+      self.id = id
